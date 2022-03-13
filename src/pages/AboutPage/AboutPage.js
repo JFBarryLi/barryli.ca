09976546f0c0e3d76578811eeb4f8893f373872c
barry <barry@barryli.ca>
2022-03-13 18:23:56 +0000

    Added AboutPage skeleton.

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import NavBar from 'components/NavBar';
+
+const AboutPage = () => (
+      display: 'block',
+      width: '100%',
+      height: '100%',
+    }}>
+      <NavBar />
+    </div>
+);
+
+export default AboutPage;
