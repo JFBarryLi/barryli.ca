768dad7675b57a3b315bc7859a0ae11681690946
barry <barry@barryli.ca>
2022-03-13 20:31:28 +0000

    Fixed AboutPage indentation.

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -3,16 +3,16 @@ import Typography from '@mui/material/Typography';
 import NavBar from 'components/NavBar';
 
 const AboutPage = () => (
-    <div sx={{
-      display: 'block',
-      width: '100%',
-      height: '100%',
-    }}>
-      <NavBar />
-      <Typography variant='h3' component='div' sx={{ flexGrow: 1 }}>
-        About
-      </Typography>
-    </div>
+  <div sx={{
+    display: 'block',
+    width: '100%',
+    height: '100%',
+  }}>
+    <NavBar />
+    <Typography variant='h3' component='div' sx={{ flexGrow: 1 }}>
+      About
+    </Typography>
+  </div>
 );
 
 export default AboutPage;
