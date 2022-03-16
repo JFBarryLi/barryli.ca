e9976422d8dea79a7b074e39d66f365ae04a433d
barry <barry@barryli.ca>
2022-03-16 16:26:59 +0000

    Added Contact content.

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,3 +1,6 @@
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
 import NavBar from 'components/NavBar';
 import PageHeader from 'components/PageHeader';
 
@@ -9,6 +12,33 @@ const ContactPage = () => (
   }}>
     <NavBar />
     <PageHeader text='Contact' />
+    <div>
+      <Typography sx={{
+        textAlign: 'center',
+        fontSize: '2em',
+        margin: '1em',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        Email me at:
+      </Typography>
+      <Link
+        href='mailto:contact@barryli.ca'
+        underline='none'
+        align='center'
+        sx={{
+          textAlign: 'center',
+          fontSize: '2em',
+          margin: '1em',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        contact@barryli.ca
+      </Link>
+    </div>
   </div>
 );
 
