58ae93ee9ac398b7da92bb1ce14bcd6f29e8452b
barry <barry@barryli.ca>
2022-03-19 12:04:19 +0000

    Made NavDrawer Swipeable.

--- src/components/NavDrawer/NavDrawer.js
+++ src/components/NavDrawer/NavDrawer.js
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -16,6 +16,9 @@ import ContactPageIcon from '@mui/icons-material/ContactPage';
 import { Link as RouterLink } from 'react-router-dom';
 
 const drawerWidth = 240;
+
+const iOS =
+    typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
 const NavDrawer = function (props) {
   const drawer = (
@@ -95,20 +98,22 @@ const NavDrawer = function (props) {
   );
 
   return (
-    <Drawer
+    <SwipeableDrawer
       variant="temporary"
       open={props.drawIsOpen}
       onClose={props.handleDrawerToggle}
       ModalProps={{
         keepMounted: true,
       }}
+      disableBackdropTransition={!iOS}
+      disableDiscovery={iOS}
       sx={{
         display: 'block',
         '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
       }}
     >
       {drawer}
-    </Drawer>
+    </SwipeableDrawer>
   );
 }
 
