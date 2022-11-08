f683511aab92dbc3c7599251cc0a15633c63e847
barry@barryli.ca <barry@barryli.ca>
2022-11-08 22:34:27 +0100

    Working request headers.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,7 +6,11 @@ import TravelPage from 'pages/TravelPage';
 import ProjectsPage from 'pages/ProjectsPage';
 import ContactPage from 'pages/ContactPage';
 
+import { useGetTravelLogByTripNameQuery } from 'services/travelLog';
+
 const Router = function() {
+  const { data, error, isLoading } = useGetTravelLogByTripNameQuery('World Tour 2021-2023');
+
   let element = useRoutes([
     { path: '/', element: <HomePage /> },
     { path: '*', element: <NotFoundPage /> },
--- src/services/travelLog.ts
+++ src/services/travelLog.ts
@@ -2,10 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const travelLogApi = createApi({
   reducerPath: 'travelLogApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.barryli.ca/travel_log/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.barryli.ca/travel_log/',
+    prepareHeaders: (headers, { getState }) => {
+      headers.set('X-Api-Key', process.env.REACT_APP_TRAVELLOG_API_KEY as string);
+      return headers
+    }
+  }),
   endpoints: (builder) => ({
     getTravelLogByTripName: builder.query({
-      query: (tripName: string) => `trip/${tripName}`,
+      query: (tripName: string) => `trips/${tripName}`,
     }),
   }),
 });
