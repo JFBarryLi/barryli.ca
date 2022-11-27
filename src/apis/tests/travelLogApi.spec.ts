c3a4bf5661559dff8e43e6d1788ad0715773dce0
barry@barryli.ca <barry@barryli.ca>
2022-11-27 17:53:53 +0100

    Revert "Updated apis to use a basic fetch wrapper instead of RTK Query."
    This reverts commit 1a3de1d0c182fb2c4759a856f78dc5b0bb5c2eee.

--- src/apis/tests/travelLogApi.spec.ts
+++
@@ -1,8 +0,0 @@
-import { getTravelLogByTripName } from 'apis/travelLog';
-
-describe('getTravelLogByTripName', () => {
-  it('should retrieve something', async () => {
-    const data = await getTravelLogByTripName('World Tour 2021-2023');
-    expect(data['Count']).toBeGreaterThan(0);
-  });
-});
