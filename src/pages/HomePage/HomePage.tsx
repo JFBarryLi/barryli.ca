852fc56f8aecaa31c0275c5f8d9b662cb25fe2d7
barry <barry@barryli.ca>
2023-11-12 05:58:27 -0500

    Removed SummaryCard from HomePage.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,7 @@
 import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
 
 import NavBar from 'components/NavBar';
-import SummaryCard from 'visuals/SummaryCard';
 import TravelGlobe from 'visuals/TravelGlobe';
 
 import { useGetTravelLogByTripNameQuery } from 'apis/travelLog';
@@ -12,13 +10,9 @@ import {
   selectTravelPaths,
   selectTravelLocations,
   selectMaxDays,
-  selectCurrentLocation,
-  selectCountryCount,
 } from 'slices/travelLog';
 
 const HomePage = () => {
-  const { isLoading } = useGetTravelLogByTripNameQuery('World Tour 2021-2023');
-
   const travelPaths = useSelector(selectTravelPaths);
   const travelLocations = useSelector(selectTravelLocations);
   const maxDays = useSelector(selectMaxDays);
@@ -28,21 +22,6 @@ const HomePage = () => {
     maxDays: maxDays,
   }
 
-  const firstDay = new Date('09/30/2021');
-  const today = new Date();
-  // The + here is to coerce the date to a number for Typescript.
-  const currentDay = Math.round((+today - +firstDay) / (1000*60*60*24));
-  const currentLocation = useSelector(selectCurrentLocation);
-  const countryCount = useSelector(selectCountryCount);
-  const summaryCardData = {
-    currentDay: currentDay,
-    currentLocation: isLoading ? <
-      LinearProgress
-      color = 'primary'
-    /> : currentLocation,
-    countryCount: countryCount,
-   }
-
   return (
     <div>
       <Box component='div' sx={{
@@ -51,7 +30,6 @@ const HomePage = () => {
         height: '100%',
       }}>
         <NavBar />
-        <SummaryCard data={summaryCardData} />
       </Box>
       <TravelGlobe data={globeData} />
     </div>
